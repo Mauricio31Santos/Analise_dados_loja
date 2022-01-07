c541fea0507ef2af1197575a4878cef23ccfae0d
mauricio31santos <mauriciobsantos31@outlook.com>
2022-01-06 23:31:20 -0300

    importando a base de dados

---
+++ faturamento_loja.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import openpyxl
+
+# import a base de dados
+tabela_vendas = pd.read_excel("Vendas.xlsx")
+print(tabela_vendas)
+
+
+# visualizar a base de dados
+
+ # faturamento por loja
+
+ # quantidade de prodtos vendidos por loja
+
+ # ticket médio por produto em cada loja
+
+ # enviar um email com relatorio
+
